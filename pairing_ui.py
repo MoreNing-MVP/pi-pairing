1f0326d3b412eface8917608a3f96770363c32db
noam <noam95@gmail.com>
2024-03-07 18:17:44 +0200

    init

---
+++ pairing_ui.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template_string
+import subprocess
+
+app = Flask(__name__)
+
+def set_wifi(ssid, password):
+    subprocess.call(['nmcli', 'dev', 'wifi', 'connect', ssid, 'password', password])
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        ssid = request.form['ssid']
+        password = request.form['password']
+        set_wifi(ssid, password)
+        return 'WiFi settings updated. Trying to connect...'
+    
+    # Form for user input
+    return render_template_string('''
+                                  <form method="post">
+                                      SSID: <input type="text" name="ssid"><br>
+                                      Password: <input type="password" name="password"><br>
+                                      <input type="submit" value="Submit">
+                                  </form>
+                                  ''')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ togle_hotspot.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Variables
+hotspot_connection_name="MoreNing"
+GPIO_PIN=17  # Change to the actual GPIO pin number
+
+# Function to check internet connectivity
+check_internet() {
+    ping -c 4 8.8.8.8 > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Internet is connected."
+        return 1
+    else
+        echo "Internet is not connected."
+        return 0
+    fi
+}
+
+# Function to check GPIO pin status
+check_gpio_pin() {
+    # Export the GPIO pin
+    echo $GPIO_PIN > /sys/class/gpio/export
+
+    # Set the GPIO pin as an input
+    echo "in" > /sys/class/gpio/gpio$GPIO_PIN/direction
+
+    button_status=$(cat /sys/class/gpio/gpio$GPIO_PIN/value)
+    echo button_status: $button_status
+    # Unexport the GPIO pin
+    echo $GPIO_PIN > /sys/class/gpio/unexport
+
+}
+
+# Main loop
+while true; do
+    check_internet
+    internet_connected=$?
+
+    # Check GPIO pin if internet is connected
+    if [ $internet_connected -eq 1 ]; then
+        gpio_status=$(check_gpio_pin)
+        if [ "$gpio_status" -eq "1" ]; then
+            echo "Waiting for 15 seconds to check pin status again..."
+            sleep 15
+            gpio_status=$(check_gpio_pin)
+            if [ "$gpio_status" -eq "1" ]; then
+                echo "Switching to hotspot mode..."
+                nmcli con up "$hotspot_connection_name"
+                systemctl start network-toggle.service
+            fi
+        fi
+    else
+        echo "Switching to hotspot mode due to no internet..."
+        nmcli con up "$hotspot_connection_name"
+        systemctl start network-toggle.service
+    fi
+
+    sleep 30 # Check every 30 seconds
+done
