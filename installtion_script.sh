26edc8db73ad981ea9794ead4a8dc05dc59a745e
Noam Kotlar <62888429+noam95@users.noreply.github.com>
2024-03-13 09:01:56 +0200

    Update installtion_script with sudo privilage

--- installtion_script.sh
+++ installtion_script.sh
@@ -4,23 +4,23 @@
 # SSID="my-hotspot"
 # PASSWORD="my-password"
 
-# Copy the service files to the systemd directory
+echo "Copy the service files to the systemd directory"
 sudo cp pairing_ui.service /etc/systemd/system/
 sudo cp network-toggle.service /etc/systemd/system/
 
-# Reload systemd to recognize the new services
+echo "Reload systemd to recognize the new services"
 sudo systemctl daemon-reload
 
-# Enable and start the services
+echo "Enable and start the services"
 sudo systemctl enable pairing_ui.service
 sudo systemctl start pairing_ui.service
 sudo systemctl enable network-toggle.service
 sudo systemctl start network-toggle.service
 
-# Create and configure the access point
-nmcli con add type wifi ifname wlan0 con-name $SSID autoconnect yes ssid $SSID
-nmcli con modify $SSID 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
-nmcli con modify $SSID wifi-sec.key-mgmt wpa-psk
-nmcli con modify $SSID wifi-sec.psk $PASSWORD
+echo "Create and configure the access point"
+sudo nmcli con add type wifi ifname wlan0 con-name $SSID autoconnect yes ssid $SSID
+sudo nmcli con modify $SSID 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+sudo nmcli con modify $SSID wifi-sec.key-mgmt wpa-psk
+sudo nmcli con modify $SSID wifi-sec.psk $PASSWORD
 
 echo "Setup completed."
